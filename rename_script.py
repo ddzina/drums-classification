80e216169d0cedbbe480b2bc4dc8ae0766f42ad3
ddzina <ddzinareal@gmail.com>
2023-02-15 16:18:18 +0300

    refactor to PEP, removed commented code

--- rename_script.py
+++ rename_script.py
@@ -2,11 +2,12 @@ import os
 
 PATH = 'Dataset'
 
+
 def main():
     for dirpath, dirnames, filenames in os.walk(PATH):
         if dirpath is not PATH:
             for i, f in enumerate(filenames):
-                os.rename(f'{os.path.join(dirpath, f)}', f'{os.path.join(dirpath, f"{dirpath[8:]} {i+1}.wav")}')
+                os.rename(f'{os.path.join(dirpath, f)}', f'{os.path.join(dirpath, f"{dirpath[8:]} {i + 1}.wav")}')
 
 
 if __name__ == '__main__':
